f687d56189dd4208cd22c682267eecaa3c8a1f95
jahson <infojahson@gmail.com>
2024-06-06 19:12:54 +0300

    - Added login page - User login functionality - Limit the companies shown on the list based on the assigned companies

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const QRCode = require('qrcode');
 const Customer = require('./models/customer');
+const User = require('./User');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
+
 const app = express();
 const port = process.env.PORT || 3003;
 
@@ -12,6 +17,19 @@ app.set('views', './views');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+var MemoryStore = session.MemoryStore;
+app.use(session({
+    secret: ['secret', 'secret', 'secret'],
+    name: "esg-benchmark",
+    cookie: { maxAge: 3600000 },
+    expires: new Date(new Date().getTime() + 3600000),//1 day session
+    resave: true,
+    store: new MemoryStore(),
+    saveUninitialized: true
+}));
+
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/my-crm-db')
@@ -25,15 +43,15 @@ mongoose.connect('mongodb://localhost/my-crm-db')
 const router = express.Router();
 
 // Define a GET route on the router
-router.get('/', async (req, res) => {
-    try {
-        const customers = await Customer.find({});
-        res.render('customers', { customers });
-    } catch (error) {
-        console.error('Failed to retrieve customers:', error);
-        res.status(500).send('Failed to retrieve customers');
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         const customers = await Customer.find({});
+//         res.render('customers', { customers });
+//     } catch (error) {
+//         console.error('Failed to retrieve customers:', error);
+//         res.status(500).send('Failed to retrieve customers');
+//     }
+// });
 
 // Use the router
 app.use('/', router); // This line applies the router to the app
@@ -64,8 +82,35 @@ app.get('/customer-form', (req, res) => {
 // Route to serve the Customer Form page
 app.get('/esg', (req, res) => {
     // res.render('esg'); // This will render the customer-form.ejs from your views folder
-    res.render('esg-test'); 
+    //get the active session
+    if (req.session.authenticated) {
+        res.render('esg-test', { user: req.session.user });
+        return
+    }
+    //if not authenticated, go to login page
+    res.redirect('/');
+
 });
+//The login page
+app.get('/', (req, res) => {
+    res.render('index');
+});
+//post request for the login page
+app.post('/', (req, res) => {
+    User.login(req.body, function (result) {
+        if (result.status == 500) {
+            res.render('index', result);
+            return
+        }
+
+        req.session.user = result.user;
+        req.session.authenticated = true;
+        res.redirect('/esg');
+
+    })
+
+});
+
 app.get('/esg1', (req, res) => {
     res.render('esg'); // This will render the customer-form.ejs from your views folder
     // res.render('esg-test');
@@ -102,4 +147,6 @@ app.delete('/delete-customer/:id', async (req, res) => {
         res.status(500).json({ message: 'Error deleting customer' });
     }
 });
+
+
 
